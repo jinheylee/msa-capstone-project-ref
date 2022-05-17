6fcdaa4be914885e3b8534d7b36530aa212b31b3
ghgus217 <ghgus217@gmail.com>
2022-05-18 08:25:12 +0900

    Create kubuctl_build.sh
    add build sh

---
+++ CICD/kubuctl_build.sh
@@ -0,0 +1,6 @@
+kubectl apply -f 0.deployment.yaml
+kubectl apply -f 1.deployment.yaml
+kubectl apply -f 2.deployment.yaml
+kubectl apply -f 3.deployment.yaml
+kubectl apply -f 4.deployment.yaml
+kubectl apply -f 5.deployment.yaml
